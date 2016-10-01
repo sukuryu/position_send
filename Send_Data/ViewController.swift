fb0a2f65d8b2c4ceb0dacf1997d7d5a032c4b3ea
菊池修也 <kikuchi_shuuya@kikuchishuuya-no-macbook-pro.local>
2016-10-01 21:38:50 +0900

    Initial Commit

---
+++ Send_Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Send_Data
+//
+//  Created by 菊池修也 on 2016/10/01.
+//  Copyright © 2016年 菊池修也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
